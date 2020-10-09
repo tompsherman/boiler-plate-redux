43116c3c2f70dfc4a7053b78c8ecc34dd57fa1b7
unknown <tompsherman@gmail.com>
2020-10-09 15:45:36 -0400

    reducer cases built

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,23 @@
 import axios from "axios"
 
-export const FETCH_START = "FETCH_START"
-export const FETCH_SUCCESS = "FETCH_SUCCESS"
-export const FETCH_FAILURE = "FETCH_FAILURE"
+export const FETCH_DATA_START = "FETCH_DATA_START"
+export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS"
+export const FETCH_DATA_FAILURE = "FETCH_DATA_FAILURE"
 export const fetchDataInfo = () => (dispatch) => {
-    dispatch({type: FETCH_START})
+    dispatch({type: FETCH_DATA_START})
     axios
         .get("http://www.url.url/getAPI")
-        .then((response)  => dispatch({type: FETCH_SUCCESS, payload: response.data}))
-        .catch((error)=> dispatch({type: FETCH_FAILURE, payload: error}))
+        .then((response)  => dispatch({type: FETCH_DATA_SUCCESS, payload: response.data}))
+        .catch((error)=> dispatch({type: FETCH_DATA_FAILURE, payload: error}))
 }
 
-export const POST_START = "POST_START"
-export const POST_SUCCESS = "POST_SUCCESS"
-export const POST_FAILURE = "POST_FAILURE"
-export const posDataInfo = (datum) => (dispatch) => {
-    dispatch({type: POST_START})
+export const POST_DATA_START = "POST_DATA_START"
+export const POST_DATA_SUCCESS = "POST_DATA_SUCCESS"
+export const POST_DATA_FAILURE = "POST_DATA_FAILURE"
+export const postDataInfo = (datum) => (dispatch) => {
+    dispatch({type: POST_DATA_START})
     axios
         .post("http://www.url.url/postAPI", datum)
-        .then((response)  => dispatch({type: POST_SUCCESS, payload: response.data}))
-        .catch((error)=> dispatch({type: POST_FAILURE, payload: error}))
+        .then((response)  => dispatch({type: POST_DATA_SUCCESS, payload: response.data}))
+        .catch((error)=> dispatch({type: POST_DATA_FAILURE, payload: error}))
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,11 @@
+import { 
+    FETCH_DATA_FAILURE, 
+    FETCH_DATA_START, 
+    FETCH_DATA_SUCCESS, 
+    POST_DATA_FAILURE, 
+    POST_DATA_START, 
+    POST_DATA_SUCCESS } from "../actions"
+
 const initialState = {
     dataInfo: [],
     isLoading: false,
@@ -5,6 +13,8 @@ const initialState = {
 }
 
 const FETCH_DATA_START = "FETCH_DATA_START"
+//******YOU WILL DELETE OLD FETCH_DATA_DATA_START 4-5 STEPS LATER*******\\
+
 
 const reducer = (state=initialState, action) => {
     switch(action.type){
@@ -13,6 +23,39 @@ const reducer = (state=initialState, action) => {
                 ...state,
                 isLoading:true,
                 errors: ""
+            }
+        case FETCH_DATA_SUCCESS:
+            return {
+                ...state,
+                dataInfo: action.payload,
+                isLoading:false,
+                errors: ""
+            }
+        case FETCH_DATA_FAILURE:
+            return {
+                ...state,
+                isLoading:false,
+                errors: ""
+            }
+        case POST_DATA_START:
+            return {
+                ...state,
+                dataInfo: [...state.dataInfo],
+                isLoading:true,
+                errors: ""
+            }
+        case POST_DATA_SUCCESS:
+            return {
+                ...state,
+                dataInfo: action.payload,
+                isLoading:false,
+                errors: ""
+            }
+        case POST_DATA_FAILURE:
+            return {
+                ...state,
+                isLoading:false,
+                errors: action.payload
             }
         default:
             return state
