1c873501f19967af514a29ae0c59642b468be4a4
unknown <tompsherman@gmail.com>
2020-10-09 10:46:37 -0400

    list actions determined

--- src/components/BlankList.jsx
+++ src/components/BlankList.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+
+import {connect} from "react-redux"
+import {fetchDataInfo} from "../actions"
 
 import BlankCard from "./BlankCard"
 
-const BlankList = () => {
+const BlankList = (props) => {
+
+    useEffect(()=> {
+        props.fetchDataInfo()
+    }, [])
+
     return (
         <div>
             <h2>this is the blank list</h2>
-            <BlankCard />
+            {
+                props.dataInfo.map(item => (
+                    <BlankCard key={item.id} dataInfo={dataInfo}/> 
+                ))
+            }
         </div>
     )
 }
-
-export default BlankList
+const mapStateToProps = (state) => {
+    return {
+        dataInfo: state.dataInfo,
+        isLoading: state.isLoading,
+        errors: state.errors
+    }
+}
+export default connect(mapStateToProps, {fetchDataInfo})(BlankList)
