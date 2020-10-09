3bfd57e1ddabce2d82b2853f596c922182c5c640
unknown <tompsherman@gmail.com>
2020-10-09 10:55:30 -0400

    form actions determined

--- src/components/BlankForm.jsx
+++ src/components/BlankForm.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {connect} from "react-redux"
+import {postDataInfo} from "../actions"
 
-const BlankForm = () => {
+const BlankForm = (props) => {
+
+    const [dataInfo, setDataInfo] = useState({
+        //any key/value pairs required to post to API, ex:
+        name: "",
+        rank: "",
+        nickname: ""
+    })
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        props.postDataInfo(dataInfo)
+    }
+
+    const inputHandler = (event) => {
+        event.preventDefault()
+        setDataInfo({...dataInfo, [event.target.name]: event.target.value})
+    }
     return (
         <div>
             <h2>this is the blank form</h2>
+            <form onSubmit={submitHandler}>
+                <input
+                    type="text"
+                    name="dataName"
+                    value={props.name}
+                    onChange={inputHandler}
+                    placeholder="enter dataName"
+                />
+                <button>submit the info</button>
+            </form>
         </div>
     )
 }
 
-export default BlankForm
+const mapStateToProps = (state) => {
+    return {
+        dataInfo: state.dataInfo,
+        isLoading: state.isLoading,
+        errors: state.errors
+    }
+}
+
+export default connect (mapStateToProps, {postDataInfo}) (BlankForm)
